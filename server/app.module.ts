c6ab9052482165b2b7335662888e99f557896f20
SrMashida <flavio.pprado4@gmail.com>
2022-10-27 16:59:19 -0300

    adjusting app module

--- server/app.module.ts
+++ server/app.module.ts
@@ -1,4 +1,4 @@
-import {Module} from '@nestjs/common';
+import {ClassSerializerInterceptor, Module} from '@nestjs/common';
 import {PassportModule} from '@nestjs/passport';
 import {ResolversModule} from "./infra/resolvers/resolvers.module";
 import {AuthUsecasesProxyModule} from "./infra/usecases-proxy/auth/auth-usecases-proxy.module";
@@ -8,6 +8,10 @@ import {ExceptionsModule} from "./infra/exceptions/exceptions.module";
 import {LoggerModule} from "./infra/logger/logger.module";
 import {EnvironmentConfigModule} from "./infra/config/environment-config/environment-config.module";
 import {UserUsecasesProxyModule} from "./infra/usecases-proxy/user/user-usecases-proxy.module";
+import {CustomHttpException} from "./infra/commons/filters/httpException.filter";
+import {TransformResponseInterceptor} from "./infra/commons/interceptors/transformResponse.interceptor";
+import {LocalStrategy} from "./infra/commons/strategies/local.strategy";
+import {JwtStrategy} from "./infra/commons/strategies/jwt.strategy";
 
 @Module({
   imports: [
@@ -21,5 +25,21 @@ import {UserUsecasesProxyModule} from "./infra/usecases-proxy/user/user-usecases
     UserUsecasesProxyModule.register(),
     AuthUsecasesProxyModule.register(),
   ],
+  providers: [
+      {
+          provide: 'APP_FILTER',
+          useClass: CustomHttpException,
+      },
+      {
+          provide: 'APP_INTERCEPTOR',
+          useClass: ClassSerializerInterceptor,
+      },
+      {
+          provide: 'APP_INTERCEPTOR',
+          useClass: TransformResponseInterceptor,
+      },
+      LocalStrategy,
+      JwtStrategy,
+    ]
 })
 export class AppModule {}
